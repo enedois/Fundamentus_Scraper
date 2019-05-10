9c2f27751ebe1c2cb9a164f75c01c49a6c5c46c3
Renan Zanesco Comoli <37589475+enedois@users.noreply.github.com>
2019-05-09 22:24:10 -0300

    Create readhtml.py

---
+++ readhtml.py
@@ -0,0 +1,28 @@
+import os
+from pyquery import PyQuery as pq
+import lxml, lxml.html
+
+def readFile(filename):
+    filehandler = open(filename)
+    print (filehandler.read())
+    filehandler.close()
+
+current_dir = os.path.dirname(__file__)
+rel_path = "TESTE"
+papel = "ITSA4"
+
+arquivo = os.path.join(current_dir, rel_path+"\\"+papel+'.html')
+print(arquivo)
+##readFile(filename) ## para ler o conteudo
+conteudo = pq(filename=arquivo)
+resultado = conteudo("td.data span")
+print(type(resultado))
+file = open(papel+'.html','wb')
+for i in resultado:
+      conteudoString = lxml.html.tostring(i)
+      file.write(conteudoString)
+      print(conteudoString)
+file.close()
+    
+
+
